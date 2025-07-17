905d636fb986408ab7e8a9e56955bfa6803bd21d
Leonardo <93210854+lele9191@users.noreply.github.com>
2025-07-17 15:27:19 +0200

    feat: jwplayer support and new improvements

--- prodotti/osteocom/videoNewFrame.js
+++ prodotti/osteocom/videoNewFrame.js
@@ -1,23 +1,17 @@
 document.addEventListener("DOMContentLoaded", async function () {
-  // const params = new URLSearchParams(window.location.search);
-  // const videoId = params.get("videoId");
-  // console.log("videoId -->" + `${videoId}`)
-  // const channelId = params.get("channelId");
-
-  // var newToken = await VideoAccessNewApi("1", channelId, videoId);
-
-  //metto direttamente il token perchè ho fatto la chiamata da Postman
-  var newToken =
-    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjaGFubmVsSWQiOiI2N2NhYTg1ZjdiNWIzZGQyOGYwNGJmZTMiLCJ2aWRlb0lkIjoiNjdjYWFjZDE3YjViM2RkMjhmMGY1NTZjIiwidXNlckFjY2Vzc0tleSI6ImV5SjBlWEFpT2lKS1YxUWlMQ0poYkdjaU9pSklVekkxTmlKOS5leUp6ZFdJaU9tNTFiR3dzSW1sa1l5STZJbkJ6YVVkQ1NFeEVlSG81YmxFeWVIcDFaRWw1UkhjaUxDSnlkV01pT2lKd1lYSjBibVZ5SWl3aWFXRjBJam94TnpReU16a3dOekF4TENKemFXUWlPaUp3WVhKMElpd2laWGh3SWpvd0xDSnBaSFVpT2lJeE1qTTBJbjAuWXQ4ejBsNUhwZGlWY1NwT01qWjlfRHVrbFJrZnp0aFlxcWFvMl95MFVRTSIsImVtYWlsIjpudWxsLCJzdWIiOm51bGwsImlkYyI6InBzaUdCSExEeHo5blEyeHp1ZEl5RHciLCJydWMiOiJwYXJ0bmVyIiwiaWF0IjoxNzQyOTEzNDE4LCJzaWQiOiJwYXJ0IiwiZXhwIjoxNzQyOTE0MzE4LCJpZHUiOiIxMjM0In0.xoKDLXqtUuQhadhUBOYZD5oeJ5NLsdXQPGVdmRa4AmA";
- 
-  console.log(`newToken ---> ${newToken}`);
   const iframe = document.getElementById("videoFrame");
 
-  iframe.style.display = "block";
-  iframe.setAttribute(
-    "src",
-    "https://osteocom.me/en/videoaccess?signature=" + newToken
-  );
-  // iframe.setAttribute("allowfullscreen");
-  // iframe.setAttribute("allow", "fullscreen");
+  const loadPlayer = () => {
+    const newToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjaGFubmVsSWQiOiI2N2NhYTg1ZjdiNWIzZGQyOGYwNGJmZTMiLCJ2aWRlb0lkIjoiNjdjYWFkMjM3YjViM2RkMjhmMTI2ODYxIiwidXNlckFjY2Vzc0tleSI6ImV5SjBlWEFpT2lKS1YxUWlMQ0poYkdjaU9pSklVekkxTmlKOS5leUp6ZFdJaU9tNTFiR3dzSW1sa1l5STZJbkJ6YVVkQ1NFeEVlSG81YmxFeWVIcDFaRWw1UkhjaUxDSnlkV01pT2lKd1lYSjBibVZ5SWl3aWFXRjBJam94TnpReU16a3dOekF4TENKemFXUWlPaUp3WVhKMElpd2laWGh3SWpvd0xDSnBaSFVpT2lJeE1qTTBJbjAuWXQ4ejBsNUhwZGlWY1NwT01qWjlfRHVrbFJrZnp0aFlxcWFvMl95MFVRTSIsImVtYWlsIjpudWxsLCJzdWIiOm51bGwsImlkYyI6InBzaUdCSExEeHo5blEyeHp1ZEl5RHciLCJydWMiOiJwYXJ0bmVyIiwiaWF0IjoxNzQ3MzIwMDM4LCJzaWQiOiJwYXJ0IiwiZXhwIjoxNzQ3MzIwOTM4LCJpZHUiOiIxMjM0In0.EOdSFbK6xE4YetMIP-Olc6MpmAqiSWnSXRJ99K9in04"; 
+    console.log(`🎬 Carico player con token: ${newToken}`);
+
+    iframe.style.display = "block";
+    iframe.setAttribute(
+      "src",
+      `https://osteocom.me/en/videoaccess2?signature=${newToken}&sublang=fr`
+    );
+  };
+
+  loadPlayer();
+  
 });
